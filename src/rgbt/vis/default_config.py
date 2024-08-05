f50a86300f9376b3257d4f9f7201180e5cdcbb5d
opacity-black <2690598117@qq.com>
2024-08-05 22:04:07 +0800

    创建了用于绘图的setting类，完成了plot绘图函数

--- src/rgbt/vis/default_config.py
+++ src/rgbt/vis/default_config.py
@@ -1,7 +1,8 @@
 from collections import namedtuple
 from numpy import ndarray
 import numpy as np
-from typing import Any
+from typing import Any,Union
+
 
 class Setting:
     def __init__(self, 
@@ -12,13 +13,42 @@ class Setting:
                 legend_loc:str = "lower left",
                 legend_fontsize:int=14,
                 legend_bold:bool=False,
+                title:Union[str, list] = "default",         # 多图时采用列表
+                title_fontsize:int=20,
+                title_bold:bool=True,
+                ) -> None:
+        self.dpi = dpi
+        self.filename = filename
+        self.legend_loc = legend_loc
+        self.title = title
+        self.legend_bold = legend_bold
+        self.fig_size = fig_size
+        self.title_fontsize = title_fontsize
+        self.legend_fontsize = legend_fontsize
+        self.title_bold = title_bold
+        
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+
+
+class PlotSetting(Setting):
+    def __init__(self, 
+                # global param
+                dpi:int = 300,
+                filename:str = "default_plot",
+                fig_size:tuple[float, float] = (6,5.5),
+                legend_loc:str = "lower left",
+                legend_fontsize:int=14,
+                legend_bold:bool=False,
                 # plot param (pr/sr/npr)
                 axis:ndarray=np.array([]),   
                 ylim:tuple[float, float]=(0.0, 1.0),
                 linewidth:int=3,
-                title:str = "default",
-                xlabel:str = "default",
-                ylabel:str = "default",
+                title:str = "title",
+                xlabel:str = "xlabel",
+                ylabel:str = "ylabel",
                 title_fontsize:int=20,
                 xlabel_fontsize:int=20,
                 ylabel_fontsize:int=20,
@@ -29,44 +59,69 @@ class Setting:
                 yticks:Any=[],
                 xtick_fontsize=None,
                 ytick_fontsize=None,
-                # radar param
+                font="TimesNewRoman",
                 ) -> None:
+        super().__init__(dpi, filename, fig_size, legend_loc, legend_fontsize, legend_bold, 
+                         title, title_fontsize, title_bold)
         self.axis = axis
         self.xticks = xticks
         self.yticks = yticks
         self.xtick_fontsize = xtick_fontsize
         self.ytick_fontsize = ytick_fontsize
         self.ylim = ylim
-        self.dpi = dpi
         self.linewidth = linewidth
-        self.filename = filename
-        self.legend_loc = legend_loc
-        self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
-        self.fig_size = fig_size
-        self.title_fontsize = title_fontsize
         self.xlabel_fontsize = xlabel_fontsize
         self.ylabel_fontsize = ylabel_fontsize
-        self.legend_fontsize = legend_fontsize
-        self.title_bold = title_bold
         self.xlabel_bold = xlabel_bold
         self.ylabel_bold = ylabel_bold
-        self.legend_bold = legend_bold
+        self.font = font
 
-    def __repr__(self) -> str:
-        return str(self.__dict__)
 
-# GlobalConfig = {
-#     "title_fontsize": 20,
-#     "x_axis_fontsize": 20,
-#     "y_axis_fontsize": 20,
-#     "legend_fontsize": 20,
-#     "title_bold": True,
-#     "x_axis_bold": True,
-#     "y_axis_bold": True,
-#     "legend_bold": False,
-# }
+
+class RadarSetting(Setting):
+    def __init__(self, 
+                 dpi: int = 300, 
+                 filename: str = "default_radar", 
+                 fig_size: tuple[float, float] = (6, 5.5), 
+                 legend_loc: str = "lower center", 
+                 legend_fontsize: int = 14, 
+                 legend_bold: bool = False, 
+                 title: str = "title", 
+                 title_fontsize: int = 20,
+                # radar param
+                attr_li=[],                     # 挑战属性列表
+                frameon:bool=False,             # 图例背景
+                enable_ticks:bool=True,         # 刻度显示
+                grid_type:str='straight',       # 网格线样式
+                fill_color:bool=True,           # 启用颜色填充
+                fill_alpha:float=0.18,          # 填充颜色的透明度
+                fill_linewidth:float=2.5,       # 填充快边界
+                fill_markersize:int=10,         # 填充边界的标记点大小
+                bbox_to_anchor:tuple=(0.5, 1.0),# 图例的相对位置
+                attr_fontsize:int=14,           # 属性字体大小
+                attr_bold:bool=False,           # 
+                rlabel_position:int=-140,       # 主轴方向
+                ytick_fontsize=None,            # 刻度字体大小
+                board:tuple=(0.75, 0.02, 0.05, 0.95, 0.37),                 # 调整边距, 上下左右+子图边距
+                 ) -> None:
+        super().__init__(dpi, filename, fig_size, legend_loc, legend_fontsize, legend_bold, title, title_fontsize)
+        self.attr_li = attr_li
+        self.frameon = frameon
+        self.enable_ticks = enable_ticks
+        self.grid_type = grid_type
+        self.fill_color = fill_color
+        self.fill_alpha = fill_alpha
+        self.bbox_to_anchor = bbox_to_anchor
+        self.attr_fontsize = attr_fontsize
+        self.attr_bold = attr_bold
+        self.rlabel_position = rlabel_position
+        self.ytick_fontsize = ytick_fontsize
+        self.board = board
+        self.fill_markersize = fill_markersize
+        self.fill_linewidth = fill_linewidth
+
 
 PR_Config = {
     "title_fontsize": 20,
@@ -102,7 +157,7 @@ def updateConfig(configName):
     config = dict_to_object(SR_Config)
 
 def get_SR_Setting():
-    SR_PlotSetting = Setting()
+    SR_PlotSetting = PlotSetting()
     SR_PlotSetting.title = "Success Plot"
     SR_PlotSetting.legend_loc = "lower left"
     SR_PlotSetting.xlabel = "overlap threshold"
@@ -111,9 +166,16 @@ def get_SR_Setting():
 
 
 def get_PR_Setting():
-    PR_PlotSetting = Setting()
+    PR_PlotSetting = PlotSetting()
     PR_PlotSetting.title = "Precision Plot"
     PR_PlotSetting.legend_loc = "lower right"
     PR_PlotSetting.xlabel = "Location error threshold"
     PR_PlotSetting.ylabel = "Precision"
     return PR_PlotSetting
+
+
+def get_Radar_Setting():
+    radarPlotSetting = RadarSetting()
+    radarPlotSetting.title = ''
+    radarPlotSetting.legend_loc = "upper center"
+    return radarPlotSetting
--- src/rgbt/vis/plot.py
+++ src/rgbt/vis/plot.py
@@ -3,23 +3,15 @@ import numpy as np
 
 from rgbt.vis.draw_utils import COLOR, LINE_STYLE
 from rgbt.vis.font import *
-from rgbt.vis.default_config import Setting
+from rgbt.vis.default_config import PlotSetting
 
-def draw_plot(result:list, setting:Setting):
-    """
-    Parameter
-    ---
-    axis: x-axis
-    result: [tracker_name - str, val - list]\r
-    fn: the image file name.
-    loc: "upper center"
-    """
+def plot(result:list[tuple[str, np.ndarray]], setting:PlotSetting):
     axis = setting.axis
     fig = plt.figure(figsize=setting.fig_size)
     ax = fig.add_subplot(111)
     for i,(name,val) in enumerate(result):
         j = i//len(COLOR)
-        ax.plot(axis, val.squeeze(), color=COLOR[i], linestyle=LINE_STYLE[j], linewidth=setting.linewidth, label=name)
+        ax.plot(axis, val, color=COLOR[i], linestyle=LINE_STYLE[j], linewidth=setting.linewidth, label=name)
     ax.set_xlim(xmax=max(axis), xmin=min(axis))
     ax.set_ylim(ymin=setting.ylim[0], ymax=setting.ylim[1])
 
@@ -39,8 +31,19 @@ def draw_plot(result:list, setting:Setting):
         ax.set_yticks(yticks)
         ax.set_yticklabels(yticks, fontsize=setting.ytick_fontsize)
 
-    ax.set_title(setting.title, fontdict=TimesNewRoman(setting.title_fontsize, setting.title_bold))
+    ax.set_title(setting.title, fontdict=TimesNewRoman(setting.title_fontsize, setting.title_bold)) # type: ignore
     ax.set_xlabel(setting.xlabel, fontdict=TimesNewRoman(setting.xlabel_fontsize, setting.xlabel_bold))
     ax.set_ylabel(setting.ylabel, fontdict=TimesNewRoman(setting.ylabel_fontsize, setting.ylabel_bold))
     ax.legend(loc=setting.legend_loc, prop=TimesNewRoman(setting.legend_fontsize, setting.legend_bold))
     fig.savefig(setting.filename, dpi=setting.dpi)
+
+
+
+if __name__=="__main__":
+    data = [
+        ("A", np.array([0.9, 0.8, 0.78, 0.75, 0.6, 0.3, 0.0])),
+        ("B", np.array([0.92, 0.82, 0.78, 0.7, 0.55, 0.28, 0.0])),
+    ]
+    setting = PlotSetting()
+    setting.axis = np.arange(7)
+    plot(data, setting)
